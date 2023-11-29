8c69e0da111b836b3ff898ed631ab42488bd1d58
fawaz <fawazyousifabkr22.com>
2023-11-29 12:44:09 -0500

    updated

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+# YOUR CODE HERE
+
+
+if (number > 0)
+    print(number + "is  poastive\n")
+else if (number < 0)
+    print(number + ' is nergatige\n')
+else 
+    print(number + ' is zero\n')
+
